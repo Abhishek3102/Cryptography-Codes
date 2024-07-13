8af27dcb277c3ef4484126b02f625f527bd72004
Abhishe16 <ankushpan694@gmail.com>
2024-07-13 11:20:37 +0530

    DES codes added

---
+++ DES/flavour2.py
@@ -0,0 +1,31 @@
+def xor(s1, s2):
+    answer = ""
+    for i in range(len(s1)):
+        if s1[i] == s2[i]:
+            answer += "0"
+        else:
+            answer += "1"
+    return answer
+
+pt = input("Plain Text (8 Bit): ")
+while len(pt) != 8:
+    print("Plain Text is not 8 bits long")
+    pt = input("Plain Text (8 Bit): ")
+
+key = input("Key (4 Bit): ")
+while len(key) != 4:
+    print("Key is not 4 bits long")
+    pt = input("Key (4 Bit): ")
+
+left = pt[:4]
+right = pt[4:]
+
+left = xor(left, key)
+right = xor(right, left)
+
+temp = left
+left = right
+right = temp
+
+ct = left + right
+print(ct)
